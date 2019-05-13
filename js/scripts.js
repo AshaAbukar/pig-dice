2f225f9b59c84b933dc7512bc7dd463b0da66396
AshaAbukar <aisha63abubakar@gmail.com>
2019-05-13 11:21:54 +0300

    Add styling to onclick buttons

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,16 @@ function Player(rollDice,turnScore,totalScore,active) {
      this.totalScore=0;
      this.active = active;
  }
+/*
+ if (random === 1) {
+         this.turnScore = 0;
+         this.rollDice = 1;
+         if (this.active === player1.active) {
+             player1.active = false;
+             player2.active = true;
+             $(".player1").children().prop('disabled', true);
+             $(".player1").addClass("disableGamingField");
+             $(".player2").children().prop('disabled', false);
+             $(".player2").removeClass("disableGamingField");
+           }
+*/
